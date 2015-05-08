5c90d0149b73e99635cee2ae4d4eb803a2ee2c67
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-08 10:48:39 -0700

    extract pipeline-inputs from upstream stub

--- production/bin/create_stub_make_manifest_and_save.sh
+++ production/bin/create_stub_make_manifest_and_save.sh
@@ -7,4 +7,27 @@ set -e
 
 cd $DIR/..
 
+
+if [[ ! -f releases/cf/version ]]; then
+  echo "missing resource input ${stage}/releases/cf/version"
+  exit 1
+fi
+CF_VERSION=$(cat releases/cf/version)
+
+if [[ ! -f stemcell/version ]]; then
+  echo "missing resource input ${stage}/stemcell/version"
+  exit 1
+fi
+STEMCELL_VERSION=$(cat stemcell/version)
+
+mkdir -p manifests
+cat >manifests/pipeline-inputs.yml <<EOF
+---
+meta:
+  release_versions:
+    cf: "$CF_VERSION"
+  stemcell:
+    version: "$STEMCELL_VERSION"
+EOF
+
 ./bin/make_manifest.sh $@ > $manifest
